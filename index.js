5660ce55362391d64b8caafc0b69e02c48b9e5a5
Bennett <bjupfield@gmail.com>
2021-11-02 16:28:50 -0500

    commit tree ac4360a555c2da3616b5326dd460a32c2ad2d59c

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin(arrys){
+    let year;
+    year = arrys.find(({result}) => result === "W");
+    if(typeof(year) == "object"){
+        return year["year"];
+    }
+    return year;
+}
